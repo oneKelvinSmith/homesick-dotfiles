9eae9cd1e8439933d06c47916a458c29cb7e88eb
Kelvin Smith <ksmith@thoughtworks.com>
2013-09-29 13:16:09 +0200

    Add gitignore. re-add emacs configuration files

--- home/.emacs.d/personal/personal-config.el
+++ home/.emacs.d/personal/personal-config.el
@@ -1,1 +1,41 @@
-/Users/kelvin/.homesick/repos/oneKelvinSmith/dotfiles/home/.emacs.d/personal/personal-config.el
+;;; personal-config.el -- personal configuration file
+
+;;; Commentary:
+
+;;; Code:
+
+;; disable scroll bars
+(scroll-bar-mode -1)
+
+;; set cursor to bar
+(setq cursor-type 'bar)
+
+;; set theme to monokai
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/monokai-emacs")
+(load-theme 'monokai t)
+
+;; swap prelude keybindings to match the rest of the system
+(setq mac-command-modifier 'super)
+(setq mac-option-modifier 'meta)
+
+;; enable auto-complete mode
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/emacs.d/dict")
+(ac-config-default)
+(global-auto-complete-mode t)
+(setq ac-auto-start nil)
+(ac-set-trigger-key "TAB")
+
+;; multiple cursors mode
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(defun in-terminal ()
+  "Return true if in a terminal."
+  (not (display-graphic-p)))
+
+(provide 'personal-config)
+;;; personal-config.el ends here
--- home/.emacs.d/personal/rails-config.el
+++ home/.emacs.d/personal/rails-config.el
@@ -1,1 +1,58 @@
-/Users/kelvin/.homesick/repos/oneKelvinSmith/dotfiles/home/.emacs.d/personal/rails-config.el
+;;; rails-config.el -- ruby on rails configuration
+;;
+;;; Commentary:
+;;  personal rails environment for Emacs using Github repositories I've forked
+;;  as well as packages included via the ELPA.
+;;
+;;; Code:
+
+;; superfluous (possibly) requirement of ruby mode
+(require 'ruby-mode)
+
+;; use 1TBS for ruby code
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  "1TBS Closing Parenthesis.
+This function cause closing parenthesis to use the K&R indent style in 'ruby-mode'."
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
+
+;; enable ido for rinari
+(require 'ido)
+(ido-mode t)
+
+;; enable rinari
+;; (add-to-list 'load-path "~/.emacs.d/vendor/rinari")
+(require 'rinari)
+(global-rinari-mode)
+
+;;; rhtml mode
+;; (add-to-list 'load-path "~/.emacs.d/vendor/rhtml")
+(require 'rhtml-mode)
+
+;; enable rspec mode
+;; (add-to-list 'load-path "~/.emacs.d/vendor/rspec")
+(require 'rspec-mode)
+
+;; enable haml mode
+(require 'haml-mode)
+
+;; enable yaml mode
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
+;; enable sass mode
+(require 'sass-mode)
+
+(provide 'rails-config)
+;;; rails-config.el ends here
